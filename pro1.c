2a29b8c5c7bcc39cfde0964402040879df5037a3
Shubham Kumar <kshubham506@gmail.com>
2019-04-19 23:00:08 +0530

    Update pro1.c

--- pro1.c
+++ pro1.c
@@ -23,7 +23,7 @@ P4 13 10
 struct process{
 	int arrivalTime;
 	int burstTime;
-	char *processName;
+	char processName[10];
 	int processNum;
 	int fetched;
 	
@@ -35,6 +35,14 @@ struct process{
 struct process pro[4]; 
 int prefinalTotal;
 int totalturnAroundTime,totalWaitTime;
+int n,minartime;
+
+
+
+void printques()
+{
+	printf("Ques. 1. Considering 4 processes with the arrival time and the burst time requirement of the \nprocesses the scheduler schedules the processes by interrupting the processor after every 3 units\nof time and does consider the completion of the process in this iteration. The schedulers then\nchecks for the number of processes waiting for the processor and allots the processor to the\nprocess but interrupting the processor after every 6 units of time and considers the completion of\nthe process in this iteration. The scheduler after the second iteration checks for the number of\nprocesses waiting for the processor and now provides the processor to the process with the least\ntime requirement to go in the terminated state.\nThe inputs for the number of requirements, arrival time and burst time should be provided by the user.\nConsider the following units for reference.\nProcess Arrival time Burst time\nP1 0 1\nP2 2 23\nP3 4 13\nP4 13 10");
+}
 
 //for maintaining the ready queue
 int queue[MAX];
@@ -68,27 +76,40 @@ int delete()
     }
     else
     {
-       // printf("\nElement deleted from queue is : %d\n", queue[front]);
+        //printf("\nElement deleted from queue is : %d\n", queue[front]);
         front = front + 1;
         return  queue[front-1];
     }
 }
-void display()
+int display()
 {
-    int i;
+    int i,ret=0;
     if (front == - 1){}
 	
        // printf("Queue is empty \n");
     else
     {
-        printf("\n\n\tReady Queue : ");
+    	int f=0,j=0;
+    	for (j = front; j <= rear; j++)
+        {
+        	if(pro[queue[j]].burstTime!=0)
+        	{
+        		printf("\n\n\tReady Queue : ");
+        		ret=1;
+        		break;
+			}
+        	
+		}
+		
+		
         for (i = front; i <= rear; i++)
         {
         	if(pro[queue[i]].burstTime!=0)
-        		printf("%d ", (queue[i]+1));
+        		printf("%s ", (pro[queue[i]].processName));
 		}
-            
+		 
     }
+    return ret;
 }
 int find(int val)
 {
@@ -108,13 +129,13 @@ int find(int val)
 }
 void insertIntoReadyQueue(int currentTime)
 {
-	int exec = 0, i; 
-    for (i = 0; i < 4; i++)
+	int i; 
+    for (i = 0; i < n; i++)
 	 { 
         
         if (pro[i].arrivalTime <= currentTime && find(i)!=1 && pro[i].fetched==0) 
         {
-        	//printf("Inserted i:%d\n",i);
+        //	printf("\nInserted i:%d , pro arrival :%d,current, : %d \n",i,pro[i].arrivalTime, currentTime);
         	insert(i); 
 		}
     } 
@@ -140,39 +161,68 @@ int findLeastBurstTime()
         		pr=queue[i];
 			}		
 		}
-	////	printf("\nLEast: %d : %d\n",pr,min);
+		//printf("\nLEast: %d : %d\n",pr,min);
 		return pr;
 }
 void findCompletionTime()
 {
 	printf("\n\n\tProcess Execeution Chart\n\t-----------------------------\n");
-	int currentTime=0,count=1;
+	int currentTime=0,count=1,execTill=0;
 	int pno,pno1;
+	//currentTime=pro[0].arrivalTime;
+	currentTime=minartime;
+	prefinalTotal+=minartime;
+	//printf("\nCurrent time :%d , Pre final : %d",currentTime,prefinalTotal);
 	insertIntoReadyQueue(currentTime);
-	while(1)
+	while( count<=20)
 	{
 		//printf("\nCuuernert time : %d\n",currentTime);
 		
-		display();
-			
+		int val= display();
+		if(val==0)
+		{
+			currentTime++;	
+			insertIntoReadyQueue(currentTime);
+			continue;
+		}
+		execTill=0;
 		if(count==1)
 		{
 			pno=pro[delete()].processNum;
-			printf("\n\nProcess executing at time %d till %d is :P %d \t", currentTime,(currentTime+3),pno ); 
-			pro[pno-1].burstTime -=3;
-			currentTime = currentTime+3;
+			if(pro[pno-1].burstTime<3)
+				execTill=pro[pno-1].burstTime;
+			else
+				execTill=3;
+		//	printf("\ncount 1 : %d , process : %s\n",execTill,pro[pno-1].processName);
+			printf("\n\nProcess executing at time %d till %d is :P %d \t", currentTime,(currentTime+execTill),pno ); 
+			pro[pno-1].burstTime -=execTill;
+			currentTime = currentTime+execTill;
 		}
 		else if(count==2)
 		{
+			
 			pno=pro[delete()].processNum;
-			printf("\n\nProcess executing at time %d till %d is :P %d \t", currentTime,(currentTime+6),pno ); 
-			pro[pno-1].burstTime -=6;
-			currentTime = currentTime+6;
+			if(pro[pno-1].burstTime<6)
+				execTill=pro[pno-1].burstTime;
+			else
+				execTill=6;
+			//printf("\ncount 2 : %d , process : %s\n",execTill,pro[pno-1].processName);
+			printf("\n\nProcess executing at time %d till %d is :P %d \t", currentTime,(currentTime+execTill),pno ); 
+			pro[pno-1].burstTime -=execTill;
+			currentTime = currentTime+execTill;
 		}
 		else
 		{
+		//	printf("\nelse\n");
 			pno1=findLeastBurstTime();
 			pno=pno1+1;
+		/*	if(pno1>=n)
+			{
+				currentTime++;	
+				insertIntoReadyQueue(currentTime);
+				continue;
+			}*/
+				
 			printf("\n\nProcess executing at time %d till %d is :P %d \t", currentTime,(currentTime+pro[pno-1].burstTime),pno ); 
 			currentTime = currentTime+pro[pno-1].burstTime;
 			pro[pno-1].burstTime =0;
@@ -198,7 +248,7 @@ void findCompletionTime()
 int findTimes()
 {
 	int i;
-	for(i=0;i<4;i++)
+	for(i=0;i<n;i++)
 	{
 		pro[i].turnaroundTime = pro[i].completionTime - pro[i].arrivalTime;
 		pro[i].waitingTime = pro[i].turnaroundTime - pro[i].tempburstTime;
@@ -209,32 +259,51 @@ int findTimes()
 
 int main()
 {	
+	//printf("Program 1 ");
+	printques();
+	//initializing all the Processes
 	
-	//initializing all the Processes(students)
-	//Time taken for taking food is the burst time 
-	//as all the students(process) arrive at the same time so it is same for all : 0
-	pro[0].arrivalTime=0;pro[0].burstTime=18;pro[0].processNum=1;pro[0].fetched=0;pro[0].processName="P1";
-	pro[1].arrivalTime=2;pro[1].burstTime=23;pro[1].processNum=2;pro[1].fetched=0;pro[1].processName="P2";
-	pro[2].arrivalTime=4;pro[2].burstTime=13;pro[2].processNum=3;pro[2].fetched=0;pro[2].processName="P3";
-	pro[3].arrivalTime=13;pro[3].burstTime=10;pro[3].processNum=4;pro[2].fetched=0;pro[3].processName="P4";
+	printf("\n\nEnter the number of processes : ");
+	scanf("%d",&n);
+	int i=0,j=0;
+	minartime=9999999;
+	for(i =0;i<n;i++)
+	{
+		printf("\nFor Process %d :\n----------------------- ",(i+1));
+		printf("\n\nEnter the Arrival Time  : ");
+		scanf("%d",&pro[i].arrivalTime);
+		if(pro[i].arrivalTime<minartime)
+			minartime=pro[i].arrivalTime;
 		
-	prefinalTotal=64;
+		printf("Enter the Burst Time : ");
+		scanf("%d",&pro[i].burstTime);
+		pro[i].processNum=(i+1);
+		pro[i].fetched=0;
+		sprintf(pro[i].processName,"P%d",(i+1));
+		
+	}
+
+/*	pro[0].arrivalTime=1;pro[0].burstTime=18;pro[0].processNum=1;pro[0].fetched=0;pro[0].processName=1;
+	pro[1].arrivalTime=2;pro[1].burstTime=23;pro[1].processNum=2;pro[1].fetched=0;pro[1].processName=2;
+	pro[2].arrivalTime=4;pro[2].burstTime=13;pro[2].processNum=3;pro[2].fetched=0;pro[2].processName=3;
+	pro[3].arrivalTime=13;pro[3].burstTime=10;pro[3].processNum=4;pro[2].fetched=0;pro[3].processName=4;*/
+		
+
 	
 	
-	printf("Program 1:\n\n\t\tGiven State: \n\nProcess \tArrivalTime\tBurstTime\n");
+	printf("\n\n\t\tGiven State: \n\nProcess \tArrivalTime\tBurstTime\n");
 	printf("------------------------------------------\n");
-	int i=0;
-	for(i=0;i<4;i++)
+	for(i=0;i<n;i++)
 	{
 		pro[i].tempburstTime=pro[i].burstTime;
 		printf("%s\t\t%d\t\t%d\n",pro[i].processName,pro[i].arrivalTime,pro[i].burstTime);
+		prefinalTotal += pro[i].burstTime;
 	}
 	
 	//sort the processes according to their process id
-	int j=0;
-	struct process temp;
-	for(i=0;i<4-1;i++){
-		for(j=i+1;j<4;j++){
+/*	struct process temp;
+	for(i=0;i<n-1;i++){
+		for(j=i+1;j<n;j++){
 			if(pro[i].arrivalTime>pro[j].arrivalTime){
 				temp=pro[i];
 				pro[i]=pro[j];
@@ -242,6 +311,16 @@ int main()
 			}
 		}
 	}
+	*/
+		
+	/*printf("\n\n\t\tAfter sorting: \n\nProcess \tArrivalTime\tBurstTime\n");
+	printf("------------------------------------------\n");
+	for(i=0;i<n;i++)
+	{
+		pro[i].tempburstTime=pro[i].burstTime;
+		printf("%s\t\t%d\t\t%d\n",pro[i].processName,pro[i].arrivalTime,pro[i].burstTime);
+		prefinalTotal += pro[i].burstTime;
+	}*/
 	
 	findCompletionTime();
 	findTimes();
@@ -250,7 +329,7 @@ int main()
 	
 	printf("---------------------------------------------------------------------------------------------\n");
 
-	for(i=0;i<4;i++){
+	for(i=0;i<n;i++){
 		printf("%s",pro[i].processName);
 		printf("\t\t%d",pro[i].arrivalTime);
 		printf("\t\t%d",pro[i].tempburstTime);
@@ -263,6 +342,6 @@ int main()
 	
 	printf("\n\nTotal Turn Around Time : %d",totalturnAroundTime);
 	printf("\nTotal Waiting Time : %d",totalWaitTime);
-	printf("\nAverage Turn Around Time : %.2f",(totalturnAroundTime/4.0));
-	printf("\nAverage Waiting Time : %.2f",(totalWaitTime/4.0));
+	printf("\nAverage Turn Around Time : %.2f",(totalturnAroundTime/(float)n));
+	printf("\nAverage Waiting Time : %.2f",(totalWaitTime/(float)n));
 }
